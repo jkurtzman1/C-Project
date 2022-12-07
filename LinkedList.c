a396cfcd0b01ae35ef4362a9bf2f95651a453c6b
jkurtzman1 <jasonkurtzman@yahoo.com>
2022-12-07 16:35:33 -0500

    Add PrintList & AppendNode
    Add functionality to print the LinkedList from the head node, and to append a node to the end of the list.

--- LinkedList.c
+++ LinkedList.c
@@ -5,10 +5,45 @@
 
 #include <stdio.h>
 
+struct node{
+    int data;
+    struct node *next;
+};
 
+void printList(struct node *head)
+{
+    while(head -> next != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("%d\n", head->data);
+}
+
+// Add node to end of list
+void appendNode(struct node *head, struct node *nodeToAdd)
+{
+    while(head->next != NULL)
+    {
+        head = head -> next;
+    }
+
+    head -> next = nodeToAdd;
+    nodeToAdd -> next = NULL;
+}
 
 int main()
 {
-    printf("Hello World!\n");
+    struct node node1 = {1, NULL};
+    printList(&node1);
+    struct node node2 = {2, NULL};
+    appendNode(&node1, &node2);
+    printList(&node1);
+    struct node node3 = {3, NULL};
+    appendNode(&node1, &node3);
+    printList(&node1);
+    struct node node4 = {4, NULL};
+    appendNode(&node1, &node4);
+    printList(&node1);
     return 0;
 }
